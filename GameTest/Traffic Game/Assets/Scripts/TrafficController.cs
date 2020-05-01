5861cc2c47493d9d66a754b288dc10c1135fed5c
PutterChez <44222382+PutterChez@users.noreply.github.com>
2020-05-01 21:41:02 +0700

    Revert "Assignment 5"
    This reverts commit 5fb563934be45a73c23b25da70c57737682531f7.

--- GameTest/Traffic Game/Assets/Scripts/TrafficController.cs
+++ GameTest/Traffic Game/Assets/Scripts/TrafficController.cs
@@ -5,47 +5,38 @@ using UnityEngine;
 public class TrafficController : MonoBehaviour {
 
 	public List<Sprite> light_sprites;
-	public Sprite green, red;
+	public Sprite off, green, yellow, red;
 
-    public int light_setting = 1;
-    private CarBotScript currentCar;
+	public int light_setting = 	0;
 
-    // Use this for initialization
-    void Start ()
-    {
-        light_sprites.Add(red);
-        light_sprites.Add(green);
-    }
+	// Use this for initialization
+	void Start () {
+		light_sprites.Add (off);
+		light_sprites.Add (green);
+		light_sprites.Add (yellow);
+		light_sprites.Add (red);
+	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		if (Input.GetMouseButtonDown (0)) {
 			Vector3 wp = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			Collider2D coll = this.gameObject.GetComponent<Collider2D> ();
 
 			if (coll.OverlapPoint (wp)) {
-                if (light_setting == 1)
-                    light_setting = 0;
-                else if (light_setting == 0)
-                    light_setting = 1;
+				
+				if (light_setting < 3)
+					light_setting++;
+				else
+					light_setting = 0;
+				
+				this.gameObject.GetComponent<SpriteRenderer> ().sprite = light_sprites[light_setting];
 			}
 		}
-        this.gameObject.GetComponent<SpriteRenderer>().sprite = light_sprites[light_setting];
-    }
+	}
 
 	public int getLightSetting(){
 		return light_setting;
 	}
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (!other.isTrigger)
-        {
-            currentCar = other.gameObject.GetComponent<CarBotScript>();
-
-            if (light_setting == 0)
-                currentCar.disableMove();
-        }
-
-    }
 }
