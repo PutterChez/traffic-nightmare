5fb563934be45a73c23b25da70c57737682531f7
PutterChez <44222382+PutterChez@users.noreply.github.com>
2020-05-01 21:38:44 +0700

    Assignment 5

--- GameTest/Traffic Game/Assets/Scripts/CarBotScript.cs
+++ GameTest/Traffic Game/Assets/Scripts/CarBotScript.cs
@@ -2,45 +2,110 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarBotScript : MonoBehaviour {
-	public float speed = 2f;
-	private float movement_horizontal = 0f, movement_vertical = 0f;
-	private bool canMove = true;
+public class CarBotScript : MonoBehaviour
+{
+  public float speed = 2f;
+  public float timeToDie = 2f;
+  public char direction;
+  public List<char> pathing = new List<char>();
+  private float movement_horizontal = 0f, movement_vertical = 0f;
+  private bool canMove = true;
 
-	private Rigidbody2D rigidBody;
-	private TrafficController current_traffic_light;
+  private Rigidbody2D rigidBody;
+  private TrafficController current_traffic_light;
+  private CarBotScript otherCar;
 
-	// Use this for initialization
-	void Start () {
-		rigidBody = GetComponent<Rigidbody2D> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//rigidBody.velocity = new Vector2 (movement_horizontal * speed, rigidBody.velocity.y);
+  // Use this for initialization
+  void Start()
+  {
+    rigidBody = GetComponent<Rigidbody2D>();
+  }
 
-		if (canMove)
-			rigidBody.velocity = new Vector2 (1 * speed, rigidBody.velocity.y);
+  // Update is called once per frame
+  void Update()
+  {
+    if (gameObject.tag == "Accident") {
+      canMove = false;
+    }
 
-		else {
-			if (current_traffic_light.getLightSetting() != 1)
-				rigidBody.velocity = new Vector2 (0f, 0f);
-			else
-				canMove = true;
-		}
-	}
+    if (canMove) {
+      move(direction);
+    }
+    else {
+        if (current_traffic_light != null) {
+            if (current_traffic_light.light_setting == 1)
+                canMove = true;
 
-	void OnTriggerEnter2D(Collider2D collider){
-		
-		current_traffic_light = collider.gameObject.GetComponent<TrafficController> ();
+            else{
+                if ((current_traffic_light.tag == "TrafficLightN") && (direction == 'n'))
+                    rigidBody.velocity = Vector2.zero;
 
-		if ((current_traffic_light != null) && (current_traffic_light.getLightSetting () == 3))
-			canMove = false;
-		else
-			canMove = true;
-	}
+                if ((current_traffic_light.tag == "TrafficLightE") && (direction == 'e'))
+                    rigidBody.velocity = Vector2.zero;
 
-	void OnCollisionStay(Collision collisionInfo){
-		Debug.Log ("Stopping");
-	}
+                if ((current_traffic_light.tag == "TrafficLightS") && (direction == 's'))
+                    rigidBody.velocity = Vector2.zero;
+
+                if ((current_traffic_light.tag == "TrafficLightW") && (direction == 'w'))
+                    rigidBody.velocity = Vector2.zero;
+            }
+        }
+
+        else {
+            rigidBody.velocity = Vector2.zero;
+        }
+    }
+  }
+
+  private void OnCollisionEnter2D(Collision2D other)
+  {
+    if (other.gameObject.tag == "Car" || other.gameObject.tag == "Accident")
+    {
+      canMove = false;
+      gameObject.tag = "Accident";
+      other.gameObject.tag = "Accident";
+      print("Car Crash!!!");
+      Destroy(gameObject, timeToDie);
+    }
+  }
+
+  private void OnTriggerEnter2D(Collider2D other) {
+    if (other.gameObject.tag == "Accident") {
+      otherCar = FindObjectOfType<CarBotScript>();
+      canMove = false;
+      Invoke("enableMove", otherCar.timeToDie);
+    }
+
+    else {
+        current_traffic_light = other.gameObject.GetComponent<TrafficController>();
+    }
+  } 
+
+  public void enableMove() {
+    canMove = true;
+  }
+
+  public void disableMove(){
+    canMove = false;
+  }
+
+    public void setDirection(char e)
+  {
+    this.direction = e;
+  }
+
+  void move(char direction)
+  {
+    float smooth = 8.0f;
+    float angle = 0.0f;
+    switch (direction)
+    {
+      case 'n': rigidBody.velocity = new Vector2(0, 1 * speed); angle = 0.0f; break;
+      case 's': rigidBody.velocity = new Vector2(0, -1 * speed); angle = 180.0f; break;
+      case 'e': rigidBody.velocity = new Vector2(1 * speed, 0); angle = 270.0f; break;
+      case 'w': rigidBody.velocity = new Vector2(-1 * speed, 0); angle = 90.0f; break;
+    }
+    Quaternion target = Quaternion.Euler(0, 0, angle);
+    transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+  }
 }
--- GameTest/Traffic Game/Assets/Scripts/TrafficController.cs
+++ GameTest/Traffic Game/Assets/Scripts/TrafficController.cs
@@ -5,38 +5,47 @@ using UnityEngine;
 public class TrafficController : MonoBehaviour {
 
 	public List<Sprite> light_sprites;
-	public Sprite off, green, yellow, red;
+	public Sprite green, red;
 
-	public int light_setting = 	0;
+    public int light_setting = 1;
+    private CarBotScript currentCar;
 
-	// Use this for initialization
-	void Start () {
-		light_sprites.Add (off);
-		light_sprites.Add (green);
-		light_sprites.Add (yellow);
-		light_sprites.Add (red);
-	}
+    // Use this for initialization
+    void Start ()
+    {
+        light_sprites.Add(red);
+        light_sprites.Add(green);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-
 		if (Input.GetMouseButtonDown (0)) {
 			Vector3 wp = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			Collider2D coll = this.gameObject.GetComponent<Collider2D> ();
 
 			if (coll.OverlapPoint (wp)) {
-				
-				if (light_setting < 3)
-					light_setting++;
-				else
-					light_setting = 0;
-				
-				this.gameObject.GetComponent<SpriteRenderer> ().sprite = light_sprites[light_setting];
+                if (light_setting == 1)
+                    light_setting = 0;
+                else if (light_setting == 0)
+                    light_setting = 1;
 			}
 		}
-	}
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = light_sprites[light_setting];
+    }
 
 	public int getLightSetting(){
 		return light_setting;
 	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.isTrigger)
+        {
+            currentCar = other.gameObject.GetComponent<CarBotScript>();
+
+            if (light_setting == 0)
+                currentCar.disableMove();
+        }
+
+    }
 }
